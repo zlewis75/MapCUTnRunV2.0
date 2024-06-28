e45431beddb6530c6f9926d30b32e6c380d8c773
zlewis75 <zacharyaustinlewis@gmail.com>
2024-06-28 12:01:21 -0400

    Create submitMappingJob.sh

---
+++ submitMappingJob.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#check for required command line argument
+
+if [[ -z "$1" ]]
+then
+		echo "
+		################
+		ERROR: You must include the path to your accession file in the command line call.
+		eg. sh submit.sh Path/To/Your/AccessionFile.txt
+		##################
+		"
+
+		exit
+fi
+
+#iterates through list of accessions and passes to mapping script
+
+fastqPath="../FastqFiles"
+outdir="../MappingOutput"
+
+mkdir ${outdir}
+mkdir ${outdir}/logs
+
+#make output file folders
+mkdir "${outdir}/TrimmedFastQs"
+mkdir "${outdir}/bamFiles"
+mkdir "${outdir}/counts"
+mkdir "${outdir}/bigWig"
+
+while read -r line
+
+	do
+	sleep 10
+	echo "$line mapping job submitted"
+	sbatch --export=ALL,accession="${line}",fastqPath="${fastqPath}",outdir="${outdir}" MapJgiSamples.sh & done <"$1"
